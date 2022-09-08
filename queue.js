5f5c10d238f20ee612692a264322d4e14b97d174
Zach Byerley <zachbyerley@gmail.com>
2022-09-08 11:20:56 -0400

    implemented queue and stack

--- queue.js
+++ queue.js
@@ -1,23 +1,44 @@
 class Queue {
-  constructor() {
-    this.element = {};
-    this.head = 0;
-    this.tail = 0;
-  }
-  enqueue(value) {
-    // add an element to our object
-  }
-  dequeue() {
-    // remove an element from our object and return the item removed
-  }
-  peak() {
-    // return the item at the front of the queue
-  }
-  get length() {
-    // return the length of the queue
-  }
-  get isEmpty() {}
-  // return a true or false value if the queue is empty
+    constructor() {
+        this.element = {};
+        this.head = 0;
+        this.tail = 0;
+    }
+    enqueue(value) {
+        //add a value to the queue
+        this.element[this.tail] = value;
+        this.tail++;
+    }
+    dequeue() {
+        //remove a value from the queue
+        if (!this.length) return;
+        const val = this.element[this.head];
+        delete this.element[this.head];
+        this.head++;
+        return val;
+    }
+    peak() {
+        // return the item at the front of the queue
+        return this.element[this.head];
+    }
+    get length() {
+        // return the length of the queue
+        return this.tail - this.head;
+    }
+    get isEmpty() {
+        // return a true or false value if the queue is empty
+        return this.head === this.tail;
+    }
 }
 
+//testing
 const myQueue = new Queue();
+console.log(myQueue.isEmpty);
+myQueue.enqueue(12);
+myQueue.enqueue(15);
+myQueue.enqueue(18);
+myQueue.enqueue(19);
+console.log(myQueue.length);
+console.log(myQueue.peak());
+console.log(myQueue.dequeue());
+console.log(myQueue.length);
--- stack.js
+++ stack.js
@@ -1,19 +1,29 @@
 class Stack {
-  constructor() {
-    this.pancake = [];
-  }
-  push(value) {
-    // add an item to the stack
-  }
-  pop() {
-    // remove and return an item from the stack
-  }
-  isempty = () => {
-    // return a true or false value if empty
-  };
+    constructor() {
+        this.pancake = [];
+    }
+    push(value) {
+        // add an item to the stack
+        this.pancake.push(value);
+    }
+    pop() {
+        // remove and return an item from the stack
+        return this.pancake.pop();
+    }
+    isempty = () => {
+        // return a true or false value if empty
+        return this.pancake.length === 0;
+    };
 
-  size = () => {
-    // return the length of the stack
-  };
+    size = () => {
+        // return the length of the stack
+        return this.pancake.length;
+    };
 }
 const myStack = new Stack();
+console.log(myStack.isempty());
+myStack.push(15);
+myStack.push(18);
+myStack.push(12);
+console.log(myStack.pop());
+console.log(myStack.size());
